fb43b15de5f18e7ee319d7a336e06902b5b442ca
Abhishek S. <abhisgr.de@gmail.com>
2025-09-09 00:17:26 +0200

    feat: Improve accessibility and styling of level select modal

--- js/components/planner/levelSelectModal.js
+++ js/components/planner/levelSelectModal.js
@@ -127,12 +127,12 @@ function renderTableRows() {
             <tr data-step-id="${i}">
                 <td>
                     <label class="switch">
-                        <input type="checkbox" class="enable-switch" checked>
+                        <input type="checkbox" id="enable-switch-${i}" class="enable-switch" checked>
                         <span class="slider round"></span>
                     </label>
                 </td>
                 <td>#${i}</td>
-                <td><input type="number" class="level-input" placeholder="e.g., 18" maxlength="2"></td>
+                <td class="level-input-cell"><input type="number" id="level-input-${i}" class="level-input" placeholder="e.g., 18" maxlength="2"></td>
                 <td class="trash-cell">
                     <button class="trash-btn icon-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
                 </td>
