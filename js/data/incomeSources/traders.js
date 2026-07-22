9e4c2b058dbd13ae00a4dc2ea7841780cfaeaaa9
Ggki <64657065+GizasV@users.noreply.github.com>
2026-07-22 22:52:42 +0200

    data(income): update gem trader ore costs (#41)
    Note: This is a temporary fix as we are looking into the frequent changes Supercell keeps making and we will likely be moving onto a customizable cost option in the future.
    
    Closes #41

--- js/data/incomeSources/traders.js
+++ js/data/incomeSources/traders.js
@@ -4,10 +4,11 @@ export const raidMedalTraderData = [
     { id: "raid_shiny_500", shiny: 500, glowy: 0, starry: 0, cost: 350, currency: "raid_medals", maxPacks: 2 },
 ];
 
+// Note: Temporary fix as we are looking into frequent changes Supercell keeps making; customizable cost options planned for the future.
 export const gemTraderData = [
-    { id: "gem_starry_15", shiny: 0, glowy: 0, starry: 15, cost: 275, currency: "gems", maxPacks: 1 },
-    { id: "gem_glowy_60", shiny: 0, glowy: 60, starry: 0, cost: 150, currency: "gems", maxPacks: 2 },
-    { id: "gem_shiny_300", shiny: 300, glowy: 0, starry: 0, cost: 150, currency: "gems", maxPacks: 5 },
+    { id: "gem_starry_15", shiny: 0, glowy: 0, starry: 15, cost: 115, currency: "gems", maxPacks: 1 },
+    { id: "gem_glowy_60", shiny: 0, glowy: 60, starry: 0, cost: 90, currency: "gems", maxPacks: 2 },
+    { id: "gem_shiny_300", shiny: 300, glowy: 0, starry: 0, cost: 75, currency: "gems", maxPacks: 5 },
 ];
 
 export const eventTraderData = [
